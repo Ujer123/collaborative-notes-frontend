0f8838cfa9763c80d8a14830ca5c3837b8ff68d3
Ujer123 <ujermohd0@gmail.com>
2025-01-30 19:26:36 +0530

    done

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Base URL for your API
+  baseURL: "https://workverse-backend.onrender.com/api" || "http://localhost:5000/api", // Base URL for your API
   timeout: 5000, // Optional: Set a timeout
   headers: {
     "Content-Type": "application/json", // Ensure proper content type
